e6d5f9a26ba3e0228bbdb7f2d2ec7392314bd9b7
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-10-10 02:21:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+
+bot = Client("Bot",
+             api_id=11781837,
+             api_hash="4e4135898bbc89c27c656aa4aeed6021",
+             bot_token="5753964641:AAHhDHJJpPf5RE_GNT09GiDwx8YqSkjWNk0"
+            )
+
+print('\n\nBot Started Successfully.\n\n')
+bot.run()
