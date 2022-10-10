cb4afbdbe753437bd523f4960cdeb155af30e19d
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-10-10 07:12:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ bot = Client("Bot",
 async def start(bot, message):
     await message.reply("Hey, I'm Alive.") 
     
-@bot.on_message(filters.command('gdtot'))
+@bot.on_message(filters.private)
 async def GDToT(bot, message):
     try:
-        link = await helper.getGDToT(helper.getProperLink(message.command[1]))
+        if not message.text.startswith('https://drive.google.com/uc?id='):
+            return
+        reply = await message.reply('Processing...')
+        link = await helper.getGDToT(helper.getProperLink(message.text))
         await message.reply(link)
+        await reply.delete()
     except Exception as e:
         await message.reply(f"**Traceback Info:**\n`{traceback.format_exc() }`\n**Error Text:**\n`{e}`")
         
